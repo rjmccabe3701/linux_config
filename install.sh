96413c9c2701b7c0b2879c03676890a369895c25
McCabe, Robert J <Robert.McCabe@rockwellcollins.com>
2015-12-18 20:48:11 -0600

    Better OS-type detection

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${DIR}/functions.sh
 install_files
 pushd ~
-curl http://j.mp/spf13-vim3 -L -o - | sh
+# curl http://j.mp/spf13-vim3 -L -o - | sh
 # sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 rm -rf ~/.oh-my-zsh
 rm -rf ~/.zshrc
@@ -14,12 +14,25 @@ echo 'setopt no_share_history' >> ~/.zshrc
 popd
 ln -sf ${DIR}/custom.zsh ~/.oh-my-zsh/lib/custom.zsh
 
-if [ $(uname -o) = "Cygwin" ]
-then
+
+case "$(uname -s)" in
+
+   Darwin)
+     echo 'Detected MAC'
+     ;;
+
+   Linux)
+     echo 'Detected Linux'
+     ;;
+
+   CYGWIN*|MINGW32*|MSYS*)
+     echo 'MS Windows'
+    # This should fix it.
     #If running in cygwin the git prompt is very slow
     # This should fix it.
     echo -e '[oh-my-zsh]\n\thide-status = 1' >> ~/.gitconfig
-fi
+     ;;
+esac
 
 env zsh
 
