bae09ada0130c000482f56c0c35ab3aa18cc5798
McCabe, Robert J <Robert.McCabe@rockwellcollins.com>
2016-08-15 23:43:21 -0500

    tmux: right click to paste

--- tmux_fixups.sh
+++ tmux_fixups.sh
@@ -10,8 +10,8 @@ command_exists() {
 
 #Allow pasting from system clipboard
 if command_exists "getclip"; then
-	#Middle click and <prefix>+C-v will paste the system clipboard
-	tmux bind-key -T root MouseDown2Pane run 'tmux set-buffer "$(getclip)"; tmux paste-buffer'
+	#Right click and <prefix>+C-v will paste the system clipboard
+	tmux bind-key -T root MouseDown3Pane run 'tmux set-buffer "$(getclip)"; tmux paste-buffer'
 	tmux bind-key -T prefix C-v run 'tmux set-buffer "$(getclip)"; tmux paste-buffer'
 else
 	tmux bind-key -t vi-copy "p" "tmux display-message 'Error! getclip not installed'"
