2a8652a0c6aad4418f7c43d7b90bab7aa1cf2c94
McCabe, Robert J <Robert.McCabe@rockwellcollins.com>
2017-12-03 20:12:09 -0600

    Remove reference to spf13

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${DIR}/functions.sh
-[ ~/.spf13-vim-3/uninstall.sh ] && rm -rf ~/.spf13-vim-3
 [ uninstall_oh_my_zsh ] || rm -rf ~/.oh-my-zsh
 remove_files
 rm -rf ~/.vim*
