aa15fc853e8a7596d66ff53cf791ad20340315cc
McCabe, Robert J <robert.mccabe@rockwellcollins.com>
2019-10-02 22:38:45 -0500

    support for new tmux version (2.9)

--- functions.sh
+++ functions.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-files=(tmux.conf gitconfig vimrc)
+files=(gitconfig vimrc)
 scripts=$(find ${DIR}/scripts -type f)
 
 function install_files()
 {
-    for f in ${files[@]}
-    do
-        ln -sf ${DIR}/${f} ~/.${f}
-    done
+   tmux_version=$(tmux -V | cut -c 6- | sed 's/[a-z]//g')
+   if [[ $(echo "$tmux_version >= 2.9" | bc) -eq 1 ]] ; then
+      ln -sf ${DIR}/tmux.conf ~/.tmux.conf
+   else
+      ln -sf ${DIR}/tmux_pre2_9.conf ~/.tmux.conf
+   fi
+   for f in ${files[@]}
+   do
+      ln -sf ${DIR}/${f} ~/.${f}
+   done
 
-    mkdir -p ~/scripts
-    for f in ${scripts[@]}
-    do
-        ln -sf ${f} ~/scripts/$(basename ${f})
-    done
+   mkdir -p ~/scripts
+   for f in ${scripts[@]}
+   do
+      ln -sf ${f} ~/scripts/$(basename ${f})
+   done
 }
 
 function remove_files()
 {
-    for f in ${files[@]}
-    do
-        rm -f ~/.${f}
-    done
+   for f in ${files[@]}
+   do
+      rm -f ~/.${f}
+   done
 
-    for f in ${scripts[@]}
-    do
-        rm -f ~/scripts/$(basename ${f})
-    done
+   for f in ${scripts[@]}
+   do
+      rm -f ~/scripts/$(basename ${f})
+   done
 }
 
 
